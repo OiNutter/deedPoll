764f0dcea6f6de7adbb1c6368eb3374f4d166bbd
OiNutter <willmckenzie@oinutter.co.uk>
2011-02-16 16:31:31 +0000

    Built basic gui with basic rename functionality

--- src/deedPoll.py
+++ src/deedPoll.py
@@ -6,6 +6,7 @@ from PyQt4 import QtCore, QtGui, uic
 
 import os
 import sys
+import string
 
 #for arg in sys.argv:
 #	print(arg)
@@ -22,6 +23,25 @@ class MainWindow(QtGui.QMainWindow):
 		super(MainWindow,self).__init__(parent)
 		QtGui.QMainWindow.__init__(self)
 		self.ui = uic.loadUi('main.ui', self)
+		
+		#set class variables
+		self.origFile = ""
+		
+		#set button events
+		self.connect(self.browseButton,QtCore.SIGNAL('clicked()'),self.showFileDialog)
+		self.connect(self.renameButton,QtCore.SIGNAL('clicked()'),self.renameFile)
+			
+	#show dialog to select initial file	
+	def showFileDialog(self):
+		#fileDialog = QtGui.QFileDialog(self)
+		#fileDialog.setFileMode(QtGui.QFileDialog.ExistingFile)
+		#file
+		self.origFile = QtGui.QFileDialog.getOpenFileName(self,'Select File',"/")
+		self.fileInput.setText(os.path.basename(self.origFile))
+		
+	#perform rename operation
+	def renameFile(self):
+		os.rename(self.origFile,os.path.dirname(self.origFile) + "/" + self.newName.text())
 		
 app = QtGui.QApplication(sys.argv)
 main = MainWindow()
