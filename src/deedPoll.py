f752785c11415ed3f04fd57dc62e59acd2355e22
OiNutter <willmckenzie@oinutter.co.uk>
2011-02-16 14:51:39 +0000

    Start work on basic ui

--- src/deedPoll.py
+++ src/deedPoll.py
@@ -1,14 +1,29 @@
 #!c:\Python31\python.exe
 #Filename: deedPoll.py
 
+#import gui libraries
+from PyQt4 import QtCore, QtGui, uic
+
 import os
 import sys
 
-for arg in sys.argv:
-	print(arg)
+#for arg in sys.argv:
+#	print(arg)
 
-target = sys.argv[1]
-name = sys.argv[2]
+#target = sys.argv[1]
+#name = sys.argv[2]
 
-os.rename(target,name);
+#os.rename(target,name);
 
+
+class MainWindow(QtGui.QMainWindow):
+
+	def __init__(self,parent=None):
+		super(MainWindow,self).__init__(parent)
+		QtGui.QMainWindow.__init__(self)
+		self.ui = uic.loadUi('main.ui', self)
+		
+app = QtGui.QApplication(sys.argv)
+main = MainWindow()
+main.show()
+sys.exit(app.exec_())
---
+++ src/main.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main.ui'
+#
+# Created: Wed Feb 16 14:30:22 2011
+#      by: PyQt4 UI code generator 4.8.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(400, 600)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.fileBox = QtGui.QGroupBox(self.centralwidget)
+        self.fileBox.setGeometry(QtCore.QRect(10, 10, 380, 61))
+        self.fileBox.setObjectName(_fromUtf8("fileBox"))
+        self.groupBox = QtGui.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(10, 80, 381, 151))
+        self.groupBox.setObjectName(_fromUtf8("groupBox"))
+        self.groupBox_2 = QtGui.QGroupBox(self.centralwidget)
+        self.groupBox_2.setGeometry(QtCore.QRect(10, 240, 381, 311))
+        self.groupBox_2.setObjectName(_fromUtf8("groupBox_2"))
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 560, 380, 31))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "deedPoll", None, QtGui.QApplication.UnicodeUTF8))
+        self.fileBox.setTitle(QtGui.QApplication.translate("MainWindow", "Select File or Directory", None, QtGui.QApplication.UnicodeUTF8))
+        self.groupBox.setTitle(QtGui.QApplication.translate("MainWindow", "Specify Options", None, QtGui.QApplication.UnicodeUTF8))
+        self.groupBox_2.setTitle(QtGui.QApplication.translate("MainWindow", "GroupBox", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "RENAME", None, QtGui.QApplication.UnicodeUTF8))
+
